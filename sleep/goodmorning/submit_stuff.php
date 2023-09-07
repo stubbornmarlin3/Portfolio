51a3b9872e54583f3c3afab9cf1757d83032aabc
stubbornmarlin3 <arcarter129@gmail.com>
2023-09-07 10:42:54 -0400

    test form submit

---
+++ sleep/goodmorning/submit_stuff.php
@@ -0,0 +1,10 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Retrieve form data
+    $name = $_POST["name"];
+
+    // Display the submitted data
+    echo "Name: " . $name . "<br>";
+   
+}
+?>
